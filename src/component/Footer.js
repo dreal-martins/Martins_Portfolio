5e5c45cae73548341cbc2530f350fcaec38d4dae
Alexander Martins <alexandermartins123@icloud.com>
2023-04-23 15:26:50 -0600

    first commit

---
+++ src/component/Footer.js
@@ -0,0 +1,32 @@
+import React from "react";
+import Layout from "./Layout";
+import Link from "next/link";
+
+const Footer = () => {
+  return (
+    <footer className="w-full border-t-2 border-solid border-dark font-meduim text-sm dark:text-light dark:border-light sm:text-base">
+      <Layout className="py-2 flex items-center justify-between lg:flex-col lg:py-6">
+        <span>{new Date().getFullYear()}&copy; All Rights Reserved.</span>
+        <div className="flex items-center lg:py-2">
+          Build With
+          <span className="text-primary dark:text-primaryDark text-2xl px-1 ">
+            &#9825;
+          </span>
+          by&nbsp;
+          <Link href="/" className="underline underline-offset-2">
+            DrealMartins
+          </Link>
+        </div>
+        <Link
+          href="mailto:alexandermartins123@icloud.com"
+          target="_blank"
+          className="underline underline-offset-2"
+        >
+          Say hello
+        </Link>
+      </Layout>
+    </footer>
+  );
+};
+
+export default Footer;
---
+++ src/pages/about.js
@@ -0,0 +1,122 @@
+import AnimatedText from "@/component/AnimatedText";
+import Layout from "@/component/Layout";
+import Head from "next/head";
+import Image from "next/image";
+import React, { useEffect, useRef } from "react";
+import profilePic from "../../public/images/profile/developer-pic-2.jpg";
+import { useInView, useMotionValue, useSpring } from "framer-motion";
+import Skills from "@/component/Skills";
+import Experience from "@/component/Experience";
+import Education from "@/component/Education";
+import TransitionEffect from "@/component/TransitionEffect";
+
+const AnimatedNumber = ({ value }) => {
+  const ref = useRef(null);
+  const motionValue = useMotionValue(0);
+  const springValue = useSpring(motionValue, { duration: 3000 });
+  const isInView = useInView(ref, { once: true });
+
+  useEffect(() => {
+    if (isInView) {
+      motionValue.set(value);
+    }
+  }, [isInView, value, motionValue]);
+
+  useEffect(() => {
+    springValue.on("change", (latest) => {
+      if (ref.current && latest.toFixed(0) <= value) {
+        ref.current.textContent = latest.toFixed(0);
+      }
+    });
+  }, [springValue, value]);
+
+  return <span ref={ref}></span>;
+};
+const about = () => {
+  return (
+    <>
+      <Head>
+        <title>Martins Portfolio❤| About Page</title>
+        <meta name="description" content="any description" />
+      </Head>
+      <TransitionEffect />
+      <main className="flex w-full flex-col items-center justify-center dark:text-light">
+        <Layout className="pt-12">
+          <AnimatedText
+            text="Passion Fuels Purpose!"
+            className="mb-16 !text-7xl lg:!text-5xl sm:!text-5xl xs:!text-4xl sm:mb-8"
+          />
+          <div className="grid w-full grid-cols-8 gap-16 sm:gap-8">
+            <div className="col-span-3 flex flex-col items-start justify-start xl:col-span-4 md:order-2 md:col-span-8">
+              <h2 className="mb-4 text-lg font-bold uppercase text-dark/75 dark:text-light/75">
+                Biography
+              </h2>
+              <p className="font-medium">
+                {`Hi, I'm Martins, a web and mobile app developer with a
+                 passion for creating beautiful, functional, and user-centered
+                 digital experiences. With almost 2 years of experience in the
+                 field. I am always looking for new and innovative ways to bring
+                 my client's visions to life.`}
+              </p>
+              <p className="my-4 font-medium">
+                {`I believe that design is about more than just making things
+                 look pretty – it's about solving problems and creating
+                 intuitive, enjoyable experiences for users.`}
+              </p>
+              <p className="font-medium">
+                {`Whether I'm working on a website, mobile app, or other
+                 digital product, I bring my commitment to design excellence and
+                 user-centered thinking to every project I work on. I look
+                 forward to the opportunity to bring my skills and passion to
+                 your next project.`}
+              </p>
+            </div>
+            <div className="col-span-3 relative h-max rounded-2xl border-2 border-solid border-dark bg-light p-8 dark:bg-dark dark:border-light xl:col-span-4 md:order-1 md:col-span-8">
+              <div className="absolute top-0 -right-3 -z-10 w-[102%] h-[103%] rounded-[2rem] bg-dark dark:bg-light " />
+              <Image
+                src={profilePic}
+                alt="martins"
+                className="w-full h-auto rounded-2xl"
+                priority
+                sizes="(max-width: 768px) 100vw,
+              (max-width: 1200px) 50vw,
+              33vw"
+              />
+            </div>
+            <div className="col-span-2 flex flex-col items-end justify-between xl:col-span-8 xl:flex-row xl:items-center md:order-3">
+              <div className="flex flex-col items-end justify-center xl:items-center">
+                <span className="inline-block text-7xl font-bold md:text-6xl sm:text-5xl xs:text-4xl">
+                  <AnimatedNumber value={`20`} />+
+                </span>
+                <h2 className="text-xl font-medium capitalize text-dark/75 dark:text-light/75 xl:text-center md:text-ld sm:text-base xs:text-sm">
+                  satisfied clients
+                </h2>
+              </div>
+              <div className="flex flex-col items-end justify-center xl:items-center">
+                <span className="inline-block text-7xl font-bold md:text-6xl sm:text-5xl xs:text-4xl">
+                  <AnimatedNumber value={`28`} />+
+                </span>
+                <h2 className="text-xl font-medium capitalize text-dark/75 dark:text-light/75 xl:text-center md:text-ld sm:text-base xs:text-sm">
+                  Projects completed
+                </h2>
+              </div>
+              <div className="flex flex-col items-end justify-center xl:items-center">
+                <span className="inline-block text-7xl  font-bold md:text-6xl sm:text-5xl xs:text-4xl">
+                  <AnimatedNumber value={`1`} />+
+                </span>
+                <h2 className="text-xl font-medium capitalize text-dark/75 dark:text-light/75 xl:text-center md:text-ld sm:text-base xs:text-sm">
+                  years of experience
+                </h2>
+              </div>
+            </div>
+          </div>
+          <Skills />
+          <Experience />
+          <Education />
+        </Layout>
+      </main>
+    </>
+  );
+};
+
+export default about;
