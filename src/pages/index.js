0442b84b96258d269fa8b8d19610b013549a150e
Alexander Martins <alexandermartins123@icloud.com>
2023-04-23 15:36:32 -0600

    first commit

--- src/pages/index.js
+++ src/pages/index.js
@@ -50,7 +50,7 @@ export default function Home() {
                   className="flex items-center bg-dark text-light p-2.5 px-6 rounded-lg text-lg font-semibold hover:bg-light hover:text-dark  border-2 border-solid border-transparent hover:border-dark dark:bg-light dark:text-dark hover:dark:bg-dark hover:dark:text-light hover:dark:border-light md:p-2 md:px-4 md:text-base"
                   download={true}
                 >
-                  Resume <LinkArrow className={"w-6 ml-1"} />
+                  Resume <LinkArrow className={"w-6 ml-1 sm:w-4 md:w-5"} />
                 </Link>
                 <Link
                   href="mailto:alexandermartins123@icloud.com"
